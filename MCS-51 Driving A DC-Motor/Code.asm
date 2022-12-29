2f2e141e44accd29563c2a50f88d5844d2b775e2
ahmed79ramdan <77209823+ahmed79ramdan@users.noreply.github.com>
2022-12-29 23:28:56 +0200

    Update Code.asm

--- MCS-51 Driving A DC-Motor/Code.asm
+++ MCS-51 Driving A DC-Motor/Code.asm
@@ -1,15 +1,19 @@
 $NOMOD51
 $INCLUDE (8051.MCU)
 
- org  0000h
-      MOV P1, #0ffh
-      loop:
-            MOV A, P1
-            MOV P0, A
-            CPL A 
-            MOV P3,A
-      jmp   loop
+ORG 0000h
+MOV P1, #0ffh
+loop:
 
+Right: MOV A,P1
+       CJNE A,#01H,Left
+       SETB P2.0
+       CLR P2.1
+       JMP loop
+       
+Left:
+       SETB P2.1
+       CLR P2.0
 
-;====================================================================
-      END
+      JMP loop
+END
